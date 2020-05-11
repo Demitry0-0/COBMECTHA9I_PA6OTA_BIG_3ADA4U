63a032c2af209f4b7df8fdf287cef7c5e912165b
Demitry0-0 <58517416+Demitry0-0@users.noreply.github.com>
2020-05-11 22:33:31 +0300

    Add files via upload

--- Большая задача по Maps API..py
+++ Большая задача по Maps API..py
@@ -45,7 +45,8 @@ pikcha = pygame.Surface((1000, 600))
 background.fill(pygame.Color('#000000'))
 manager = pygame_gui.UIManager((1000, 600))
 ultralist = ['sat', 'map', 'sat,skl']
-pygame_gui.elements.UITextBox(relative_rect=pygame.Rect((600, 80), (400, 100)), manager=manager, html_text='')
+pygame_gui.elements.UITextBox(relative_rect=pygame.Rect((600, 80), (400, 100)), manager=manager,
+                              html_text='')
 hello_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((600, 0), (100, 30)),
                                             text='Слой',
                                             manager=manager)
@@ -55,7 +56,8 @@ hai_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((700, 0), (1
 ha1i_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((800, 0), (100, 30)),
                                            text='Сброс',
                                            manager=manager)
-hai_textbox = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect((600, 30), (400, 30)), manager=manager)
+hai_textbox = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect((600, 30), (400, 30)),
+                                                  manager=manager)
 clock = pygame.time.Clock()
 is_running = True
 
@@ -63,8 +65,8 @@ ll = [133.795384, -25.694768]
 check = ''
 while is_running:
     response = None
-    map_request = f"https://static-maps.yandex.ru/1.x/?ll={ll[0]},{ll[1]}&spn=0," + str(mashtag).replace(".",
-                                                                                                         ".") + "&l=" + typ
+    map_request = f"https://static-maps.yandex.ru/1.x/?ll={ll[0]},{ll[1]}&spn=0," + str(
+        mashtag) + "&l=" + typ
     time_delta = clock.tick(60) / 1000.0
     if map_request != check:
         response = requests.get(map_request)
@@ -79,9 +81,16 @@ while is_running:
             is_running = False
         if event.type == pygame.KEYDOWN:
             if event.__dict__['key'] == 280:
-                mashtag += 1 if mashtag < 70 else 0
+                if mashtag > 0.010:
+                    mashtag += 0.09
+                elif mashtag > 0.000007:
+                    mashtag += 0.0009
             if event.__dict__['key'] == 281:
-                mashtag -= 1 if mashtag > 0.1 else 0
+                if mashtag > 0.011:
+                    mashtag -= 0.09
+                elif mashtag > 0.00083:
+                    mashtag -= 0.0009
+
             if event.__dict__['key'] == 276:  # left
                 ll[0] -= mashtag / 10
             if event.__dict__['key'] == 273:  # up
@@ -110,25 +119,31 @@ while is_running:
                             json_response = requests.get(geocoder_request).json()
                             try:
                                 toponym = \
-                                    json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
-                                toponym_address = toponym["metaDataProperty"]["GeocoderMetaData"]["text"]
-                                pygame_gui.elements.UITextBox(relative_rect=pygame.Rect((600, 80), (400, 100)),
-                                                              manager=manager, html_text=toponym_address)
+                                    json_response["response"]["GeoObjectCollection"][
+                                        "featureMember"][0]["GeoObject"]
+                                toponym_address = toponym["metaDataProperty"]["GeocoderMetaData"][
+                                    "text"]
+                                pygame_gui.elements.UITextBox(
+                                    relative_rect=pygame.Rect((600, 80), (400, 100)),
+                                    manager=manager, html_text=toponym_address)
                             except:
-                                pygame_gui.elements.UITextBox(relative_rect=pygame.Rect((600, 80), (400, 100)),
-                                                              manager=manager, html_text='Адрес не найден')
+                                pygame_gui.elements.UITextBox(
+                                    relative_rect=pygame.Rect((600, 80), (400, 100)),
+                                    manager=manager, html_text='Адрес не найден')
                             insearch = True
                             xj = copy.copy(ll)
                         else:
                             hai_textbox.text = 'Такого посёлка нет на етой планете'
-                            pygame_gui.elements.UITextBox(relative_rect=pygame.Rect((600, 80), (400, 100)),
-                                                          manager=manager, html_text='')
+                            pygame_gui.elements.UITextBox(
+                                relative_rect=pygame.Rect((600, 80), (400, 100)),
+                                manager=manager, html_text='')
                             insearch = False
             if event.user_type == 'ui_button_pressed':
                 if event.ui_element == ha1i_button:
                     insearch = False
-                    outbox = pygame_gui.elements.UITextBox(relative_rect=pygame.Rect((600, 80), (400, 100)),
-                                                           manager=manager, html_text='')
+                    outbox = pygame_gui.elements.UITextBox(
+                        relative_rect=pygame.Rect((600, 80), (400, 100)),
+                        manager=manager, html_text='')
                     response = requests.get(map_request)
                     map_file = "map.png"
                     if not insearch:
